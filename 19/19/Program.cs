a580f94f5bae4c7dc8bbc821415286b891e59e66
gagichce <gagichce@mcmaster.ca>
2014-01-24 00:30:30 -0500

    19 solved. should have actually read the problem statement before jumping into coding

--- 19/19/Program.cs
+++ 19/19/Program.cs
@@ -10,6 +10,19 @@ namespace _19
     {
         static void Main(string[] args)
         {
+            int numSundays = 0;
+            for (int year = 1901; year < 2001; year++)
+            {
+                for (int month = 1; month < 13; month++)
+                {
+                    if ((new DateTime(year, month, 1)).DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        numSundays++;
+                    }
+                }
+            }
+            Console.WriteLine("There were " + numSundays + " sundays in the set time");
+            Console.ReadKey(true);
         }
     }
 }
